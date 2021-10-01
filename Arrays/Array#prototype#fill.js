152121383db30d3941257055e914acea6d9405cb
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 22:49:13 +0300

    Update Array#prototype#fill.js

--- Arrays/Array#prototype#fill.js
+++ Arrays/Array#prototype#fill.js
@@ -7,21 +7,21 @@
 *   - end: The final index to fill the array with elements with. (Not included - Default: this.length)
 */
 
-Array.prototype.fillRemade = function(value, start = 0, end = this.length) {
+Array.prototype.fillRecreated = function(value, start = 0, end = this.length) {
     return this.map((elem, i) => i >= start && i < end ? value : elem)
 }
 
 /* Example */
 const arr = [1, 2, 3, 8, 10]
 
-let filledArr = arr.fillRemade(0, 1, 3)
+let filledArr = arr.fillRecreated(0, 1, 3)
 console.log(filledArr) // [ 1, 0, 0, 0, 10 ]
 
-filledArr = arr.fillRemade(0, 1)
+filledArr = arr.fillRecreated(0, 1)
 console.log(filledArr) // [ 1, 0, 0, 0, 0 ]
 
-filledArr = arr.fillRemade(0, 0, 3)
+filledArr = arr.fillRecreated(0, 0, 3)
 console.log(filledArr) // [ 0, 0, 0, 8, 10 ]
 
-filledArr = arr.fillRemade(0)
+filledArr = arr.fillRecreated(0)
 console.log(filledArr) // [ 0, 0, 0, 0, 0 ]
