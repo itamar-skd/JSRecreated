22e06dad75b35c68ab0622186cc109d28bb312f1
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 22:03:09 +0300

    Create Array#prototype#concat.js

---
+++ Arrays/Array#prototype#concat.js
@@ -0,0 +1,22 @@
+/**
+* Array.prototype.concat()
+* Description: Merges two or more arrays together.
+* Parameters:
+*   - Unlimited values to be entered. Values may be arrays or regular variables.
+*/
+
+Array.prototype.concatRemade = function() {
+    let concat = this
+    Object.values(arguments).forEach(elem => {
+        if (Array.isArray(elem)) concat = [...concat, ...elem]
+        else concat = [...concat, elem]
+    })
+
+    return concat
+}
+
+/* Example */
+const arr = [1, 2, 3]
+const arr2 = [4, 5, 6]
+const concatted = arr.concat(arr2, 'test')
+console.log(concatted) // [1, 2, 3, 4, 5, 6, 'test']
