93cafbf7b8b97a9ed52cd4df57221cedad2e94c6
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 19:21:32 +0300

    Create Array#prototype#filter.js

---
+++ Arrays/Array#prototype#filter.js
@@ -0,0 +1,16 @@
+/**
+* Array.prototype.filter()
+* Description: Returns a filtered array based on a given condition.
+* Parameters:
+*   - callback: The callback function used to return a filtered array based on a condition. The callback function takes in 3 parameters - The element currently being reviewed for the condition, its index, and the initial array.
+*   - thisValue: The value passed in for the value of the this keyword in the callback function.
+*/
+
+Array.prototype.filter = function(callback) {
+    let filtered = []
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i)) filtered.push(this[i])
+    }
+
+    return filtered
+}
