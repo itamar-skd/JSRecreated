d85e1889f8bc2f64deb5ba4161dccf4ade7783e8
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 22:30:41 +0300

    Update Array#prototype#map.js

--- Arrays/Array#prototype#map.js
+++ Arrays/Array#prototype#map.js
@@ -5,7 +5,7 @@
 *   - callback: The callback function used to return a new element based on a condition. The callback function takes in 3 parameters - The element currently being modified, its index, and the initial array.
 *   - thisValue: The value passed in for the value of the this keyword in the callback function.
 */
-Array.prototype.mapRemade = function(callback, thisValue) {
+Array.prototype.mapRecreated = function(callback, thisValue) {
     let map = []
     for (let i = 0; i < this.length; i++) {
         map.push(callback.bind(thisValue)(this[i], i, this))
