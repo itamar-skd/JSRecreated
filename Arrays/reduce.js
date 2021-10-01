f7f7712144f7ef5f62e6ff9c4188b9adc316de1e
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 22:19:52 +0300

    Create reduce.js

---
+++ Arrays/reduce.js
@@ -0,0 +1,22 @@
+/**
+* Array.prototype.reduce()
+* Description: Reduces all array values into a single value according to a given condition.
+* Parameters:
+*   - callback: The callback function used to return a new value based on a given series of operations. The callback function takes in 4 parameters - The previous value, The element currently being modified, its index, and the initial array.
+*   - initialValue: A value to represent previousValue at the beginning of the operation.
+*/
+
+Array.prototype.reduceRemade = function(callback, initialValue = this[0]) {
+    let currentValue = initialValue
+    const start = initialValue === this[0] ? 1 : 0
+    for (let i = start; i < this.length; i++) {
+        currentValue = callback(currentValue, this[i], i, this)
+    }
+
+    return currentValue
+}
+
+/* Example */
+const arr = [1, 2, 3]
+const sum = arr.reduceRemade((a, b) => a + b)
+console.log(sum) // 6
