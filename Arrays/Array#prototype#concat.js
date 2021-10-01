680550e18eb8627ac1095ef061adf1556ebc4153
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 22:30:21 +0300

    Update Array#prototype#concat.js

--- Arrays/Array#prototype#concat.js
+++ Arrays/Array#prototype#concat.js
@@ -5,7 +5,7 @@
 *   - Unlimited values to be entered. Values may be arrays or regular variables.
 */
 
-Array.prototype.concatRemade = function() {
+Array.prototype.concatRecreated = function() {
     let concat = this
     Object.values(arguments).forEach(elem => {
         if (Array.isArray(elem)) concat = [...concat, ...elem]
@@ -18,5 +18,5 @@ Array.prototype.concatRemade = function() {
 /* Example */
 const arr = [1, 2, 3]
 const arr2 = [4, 5, 6]
-const concatted = arr.concat(arr2, 'test')
+const concatted = arr.concatRecreated(arr2, 'test')
 console.log(concatted) // [1, 2, 3, 4, 5, 6, 'test']
