2ed6d2bf9a926b3ab9d4e2d99b03b627a51eb9b2
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-01 19:20:21 +0300

    Create Array#prototype#map.js

---
+++ Arrays/Array#prototype#map.js
@@ -0,0 +1,15 @@
+/**
+* Array.prototype.map
+* Description: Returns a modified array based on a given condition.
+* Parameters:
+*   - callback: The callback function used to return a new element based on a condition. The callback function takes in 3 parameters - The element currently being modified, its index, and the initial array.
+*   - thisValue: The value passed in for the value of the this keyword in the callback function.
+*/
+Array.prototype.map = function(callback, thisValue) {
+    let map = []
+    for (let i = 0; i < this.length; i++) {
+        map.push(callback.bind(thisValue)(this[i], i, this))
+    }
+
+    return map
+}
