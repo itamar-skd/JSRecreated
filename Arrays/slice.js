7e7d95d01440fa1fa5999a3f34d9d04b90597f0a
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-10-02 21:00:38 +0300

    Create slice.js

---
+++ Arrays/slice.js
@@ -0,0 +1,22 @@
+/**
+* Array.prototype.slice()
+* Extracts a section of a called array.
+* @param {Number} start Beggining index from which to start extracting the array.
+* @param {Number} end Ending index from which to end extracting the array. (Not Included)
+* @return {Array} A new array of the extracted section of the called array.
+*/
+
+Array.prototype.sliceRecreated = function(start = 0, end = this.length) {
+    let final = []
+    for (let i = start; i < end; i++) {
+        final += [...final, this[i]]
+    }
+
+    return final
+}
+
+/* Example */
+const arr = ['The', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog.']
+console.log(arr.slice()) // ['The', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog.']
+console.log(arr.slice(2, 4)) // [ 'brown', 'fox' ]
+console.log(arr.slice(5)) // [ 'over', 'the', 'lazy', 'dog.' ]
